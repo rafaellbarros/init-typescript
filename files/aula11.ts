286eb85f457ac8bc5a2d4f87c9b39cf502e61c18
Rafael <rafaelbarros.df@gmail.com>
2018-03-25 20:20:49 -0300

    Working module

---
+++ files/aula11.ts
@@ -0,0 +1,18 @@
+module App{
+    
+        export interface ICar{
+            name: string;
+        }
+  
+}
+module App{
+    
+    class Car implements ICar{
+        name:string;
+        color:string;
+    }
+    
+    var mustang = new Car();
+    console.log(mustang);
+    
+}
